9474d4dde808c66efa761a9960c50d52ff56682c
Jhon <jhoncastrillon9@gmail.com>
2018-03-16 00:09:27 -0500

    Initial commit

---
+++ SQL_Insertar Datos.sql
@@ -0,0 +1,61 @@
+GO
+USe inmoBD
+
+GO
+Insert Into CategoriaInmuebles Values ('Apartamento'),
+('Casa'), 
+('Apartaestudio'), 
+('Bodega'), 
+('Finca'),
+('Local'), 
+('Oficina')
+GO
+GO
+
+Insert Into Municipios Values 
+('Medellin'),
+('Bello'),
+('Caldas'),
+('Envigado'),
+('Itagui'),
+('La estrella')
+GO
+
+Insert Into ZonasMunicipios Values 
+('Barrio Popular',1),
+('Santa Cruz',1),
+('Manrique',1),
+('Aranjuez',1),
+('Castilla',1),
+('Doce de Octubre',1),
+('Robledo',1),
+('Villa Hermosa',1),
+('Buenos Aires',1),
+('La Candelaria',1),
+('Laureles-Estadio',1),
+('La América',1),
+('San Javier',1),
+('El Poblado',1),
+('Guayabal',1),
+('General',2),
+('General',3),
+('General',4),
+('General',5),
+('General',6)
+GO
+
+Insert Into EstadosCuentas values 
+('Pagada'),
+('Por Pagar'),
+('Anulada')
+GO
+
+Insert Into RolesUsuarios values 
+('Agente'),
+('Caja'),
+('Admin'),
+('SuperAdmin')
+GO
+
+
+
